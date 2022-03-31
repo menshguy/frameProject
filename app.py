034aa1892179647b844734ec5fc7b8871e0ff760
Jeff Fenster <jeff@untapt.com>
2022-03-30 20:29:00 -0400

    another test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 # 2. COMMENT OUT THE "Device.pin_factory = MockFactory()" LINE BELOW!
 # 4. YOU MUST COMMENT OUT THE TEST BUTTONS IN THE INDEX.HTML
 
-print("App.py starts...")
+print("App.py starts updated...")
 
 from http.client import MULTI_STATUS
 from flask import Flask, request, jsonify, render_template
