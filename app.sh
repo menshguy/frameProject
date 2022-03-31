438238ffb9deae9df7d4571f6c989af6dee0b5aa
Jeff Fenster <jeff@untapt.com>
2022-03-30 23:44:03 -0400

    a few more echo statements for future debugging

--- app.sh
+++ app.sh
@@ -1,9 +1,12 @@
 echo "app.sh runs..."
 
 # Pull lastest code from Github
+echo "pulling from github..."
 cd /home/pi/Documents/frame_project_sockets && /usr/bin/git pull https://github.com/menshguy/frameProject.git frame_valentinesDay_subway
+echo "done pulling from github"
 
 # @TODO- Copy Files from Flashdrove?
 
 # Start Flask App
+echo "Starting Flask App..."
 /home/pi/Documents/frame_project_sockets/venv/bin/python3 /home/pi/Documents/frame_project_sockets/app.py
